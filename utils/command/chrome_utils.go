e822dedf8dff7df3c0adb2c3dc02089f8adf5b11
impassive <tingfa.yang@duitang.com>
2023-07-25 18:36:12 +0800

    fix

--- utils/command/chrome_utils.go
+++ utils/command/chrome_utils.go
@@ -4,14 +4,17 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/spf13/cobra"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"net"
 	"os/exec"
 	"strconv"
+	"time"
 )
 
 type applicationInfo struct {
@@ -23,6 +26,14 @@ type bindingInfo struct {
 	Port int
 	Ip   string
 }
+
+const (
+	IP1 = "10.200.67.5"
+	IP2 = "10.200.67.6"
+	IP3 = "10.200.67.10"
+)
+
+var IPS = [3]string{IP1, IP3, IP2}
 
 func ArmeriaDebugCommand() *cobra.Command {
 	var projectName string
@@ -47,6 +58,52 @@ func ArmeriaDebugCommand() *cobra.Command {
 }
 
 func openWithProjectName(name string) {
+	bindingInfo := queryBindingInfoFromFile(name)
+	if bindingInfo == nil {
+		bindingInfo = queryBindingInfoFromMongo(name)
+	}
+	openChromeNewTab(bindingInfo.Ip, bindingInfo.Port)
+}
+
+func queryBindingInfoFromFile(name string) *bindingInfo {
+	path := "/Users/reasonknow/Downloads/应用信息规划表.xlsx"
+	excelFile, err := excelize.OpenFile(path)
+	if err != nil {
+		return nil
+	}
+
+	sheetName := excelFile.GetSheetName(1)
+	rows := excelFile.GetRows(sheetName)
+	for _, row := range rows {
+		if row[1] == name {
+			port, err := strconv.Atoi(row[4])
+			if err != nil {
+				fmt.Println("转换失败")
+				return nil
+			}
+			for _, ip := range IPS {
+				if checkIpAndPort(ip, port) {
+					return &bindingInfo{
+						Port: port,
+						Ip:   ip,
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func checkIpAndPort(ip string, port int) bool {
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", ip, port), 3*time.Second)
+	if err != nil {
+		return false
+	}
+	defer conn.Close()
+	return true
+}
+
+func queryBindingInfoFromMongo(name string) *bindingInfo {
 	// 连接MongoDB
 	clientOptions := options.Client().ApplyURI("mongodb://10.200.48.16:27017")
 	client, err := mongo.Connect(context.Background(), clientOptions)
@@ -66,7 +123,7 @@ func openWithProjectName(name string) {
 	if err != nil {
 		log.Fatal("查询 mongo 绑定信息失败")
 	}
-	openChromeNewTab(bindingInfo.Ip, bindingInfo.Port)
+	return &bindingInfo
 }
 
 func openChromeNewTab(ip string, port int) {
